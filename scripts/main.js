dc081047428f33f11bc20bc67cd07edc58baf5a3
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-05-17 15:10:22 -0400

    updated homework

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+
+document.querySelector('button').addEventListener('click', function(){
+  document.querySelector('#daImage').src="https://i.pinimg.com/originals/b9/56/59/b9565987b76d3f6819fa9931944f6c62.jpg";
+})
+
+
+
+//
+// function changePicture () {
+//
+// }
